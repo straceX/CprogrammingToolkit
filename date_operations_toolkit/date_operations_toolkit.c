5b30943dbec02898ab91b875e28e96efdfe25789
straceX <netsecdeveloper@gmail.com>
2016-05-19 15:44:36 +0300

    code review

--- date_operations_toolkit/date_operations_toolkit.c
+++ date_operations_toolkit/date_operations_toolkit.c
@@ -1,9 +1,9 @@
 int is_leap_year(unsigned int year)
 {
-	if( !year )				return 0;
-	else if( !(year%400) )	return 1;
-	else if( !(year%100) )	return 0;
+	if( !year )			return 0;
+	else if( !(year%400) )		return 1;
+	else if( !(year%100) )		return 0;
 	else if( !(year%4) 	 )	return 1;
-	else 					return 0;
+	else 				return 0;
 
 }
