ef0504aa96bcd7d2cb8b84d31ce33f3d01156df6
straceX <netsecdeveloper@gmail.com>
2016-05-22 18:07:42 +0300

    sentence tokinizing functions

--- char_array_operations_toolkit/char_array_toolkit.c
+++ char_array_operations_toolkit/char_array_toolkit.c
@@ -38,3 +38,169 @@ int sizeof_string(const char *ptrVal)
 	return iter;
 
 }
+
+int atom_number(const char *ptrSentence,const char separator_type)
+{
+
+    int length=0;
+    int ival=0;
+
+    if(*ptrSentence == separator_type)
+        goto First;
+
+    while(*ptrSentence != NULL)
+    {
+        if (*ptrSentence == separator_type)
+        {
+           ++ival;
+
+First:
+            while(*ptrSentence == separator_type || *ptrSentence == NULL)
+                ++ptrSentence;
+
+            if(*ptrSentence == NULL)
+                goto Last;
+
+        }
+        ++ptrSentence;
+    }
+
+
+Last:
+
+    return ival;
+
+}
+
+
+int max_word_length(const char *ptrSentence,const char separator_type)
+{
+
+    int max_word_length=0;
+    int length=0;
+
+    if(*ptrSentence == separator_type)
+        goto First;
+
+    while(*ptrSentence != NULL)
+    {
+        if (*ptrSentence == separator_type)
+        {
+            if(length>max_word_length)
+                max_word_length=length;
+
+            length=0;
+
+First:
+            while(*ptrSentence == separator_type || *ptrSentence == NULL)
+                ++ptrSentence;
+
+            if(*ptrSentence == NULL)
+                goto Last;
+
+        }
+        else
+            ++length;
+
+        ++ptrSentence;
+
+    }
+
+    if(length>max_word_length)
+        max_word_length=length;
+
+Last:
+
+    return max_word_length;
+
+}
+
+
+Tokenizing_Property *sentence_tokenization(const char *ptrSentence,const char separator_type)
+{
+    static Tokenizing_Property tp;
+    int max_word_length=0;
+    int length=0;
+    int ival=0;
+
+    if(*ptrSentence == separator_type)
+        goto First;
+
+    while(*ptrSentence != NULL)
+    {
+        if (*ptrSentence == separator_type)
+        {
+            if(length>max_word_length)
+                max_word_length=length;
+
+            length=0;
+            ++ival;
+
+
+First:
+            while(*ptrSentence == separator_type || *ptrSentence == NULL)
+                ++ptrSentence;
+
+            if(*ptrSentence == NULL)
+                goto Last;
+
+        }
+        else
+            ++length;
+
+        ++ptrSentence;
+
+    }
+
+    if(length>max_word_length)
+        max_word_length=length;
+
+Last:
+    tp.atom_count = ival;
+    tp.max_atom_length = max_word_length;
+
+    return &tp;
+}
+
+
+char ** separation_sentence_words(const char *ptrSentence,const char separator_type)
+{
+    int ival=1;
+    int i,j;
+    char **ptr = NULL;
+    int max_word_length=0;
+
+
+
+    if( !(ptr = (char **) ( malloc( sizeof(char*) * ival ) )) )
+            return NULL;
+    else
+    {
+        for(i=0;i<ival;++i)
+        {
+
+            if (!(ptr[i]=malloc(sizeof(char)*max_word_length)))
+            {
+                //error_message(__FILE__,__FUNC__,__LINE__);
+                return NULL;
+                
+            }
+
+        }
+        for(i=0;i<ival;++i)
+        {
+            j=0;
+
+            while ( (*ptrSentence) != ' ' && (*ptrSentence) != '\0')
+            {
+                ptr[i][j++] = (*ptrSentence);
+                ++ptrSentence;
+
+            }
+            ++ptrSentence;
+        }
+
+    }
+
+    return ptr;
+}
--- char_array_operations_toolkit/char_array_toolkit.h
+++ char_array_operations_toolkit/char_array_toolkit.h
@@ -4,3 +4,16 @@ char *upper_string(char *, size_t );
 char *lower_string(char *, size_t);
 
 int sizeof_string(const char *);
+
+
+typedef  struct Tokenizing_Property_Pair
+{
+   int max_atom_length;
+   int atom_count;
+
+}Tokenizing_Property;
+
+int atom_number(const char *,const char );
+int max_word_length(const char *,const char);
+char ** separation_sentence_words(const char *,const char );
+
