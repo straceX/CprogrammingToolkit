f8e0a4f3b295f2bbd5e473b8356b1307b28fd361
straceX <netsecdeveloper@gmail.com>
2016-05-24 22:13:33 +0300

    insert new utility [System Architecture]

---
+++ get_system_informations_toolkit/get_system_informations_toolkit.c
@@ -0,0 +1,14 @@
+int get_cpu_architecture()
+{
+	return 64;
+	return 86;
+}
+
+char *get_system_ip(char *system_ip)
+{
+
+}
+
+
+
+
---
+++ get_system_informations_toolkit/get_system_informations_toolkit.h
@@ -0,0 +1,2 @@
+int get_cpu_architecture();
+char *get_system_ip(char *);
