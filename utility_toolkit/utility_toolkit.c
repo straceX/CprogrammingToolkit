cca8c7dbfe89ec05b9886ded11a1ed8a78408b15
straceX <netsecdeveloper@gmail.com>
2016-05-30 13:13:48 +0300

    Games in C language AND inverse number template function added

--- utility_toolkit/utility_toolkit.c
+++ utility_toolkit/utility_toolkit.c
@@ -1,8 +1,16 @@
 void swap2int(int *val1,int *val2)
 {
+	if(*val1 != *val2)
+	{
+	    *val1 = (*val1) ^ (*val2);
+	    *val2 = (*val1) ^ (*val2);
+	    *val1 = (*val1) ^ (*val2);
+	}
 
-    *val1 = (*val1) ^ (*val2);
-    *val2 = (*val1) ^ (*val2);
-    *val1 = (*val1) ^ (*val2);
+}
 
+int inverse_number(int val)
+{
+
+	return val;
 }
--- utility_toolkit/utility_toolkit.h
+++ utility_toolkit/utility_toolkit.h
@@ -1,1 +1,2 @@
 void swap2int(int *,int *);
+int inverse_number(int);
