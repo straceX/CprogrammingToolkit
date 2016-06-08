ed461883dcc01ca451d9d7e2ce8f80deebc8be60
0x80 <netsecdeveloper@gmail.com>
2016-06-07 17:58:00 -0700

    HEX | ASCII conversion template added
    HEX | ASCII conversion template added

--- utility_toolkit/utility_toolkit.c
+++ utility_toolkit/utility_toolkit.c
@@ -19,15 +19,26 @@ char * base64_decoder(const char *sval)
 {
 	return NULL;
 }
-char * base64_encoder(const char *sval)
+char * base64_encoder(const char *ptrVal)
 {
 	return NULL;
 }
-char * url_decoder(const char *sval)
+char * url_decoder(const char *ptrVal)
 {
 	return NULL;
 }
-char * url_encoder(const char *sval)
+char * url_encoder(const char *ptrVal)
 {
 	return NULL;
 }
+
+char *ascii2hex(const char  *ptrVal,size_t size)
+{
+	return NULL;
+}
+char *hex2ascii(const char  *ptrVal,size_t size)
+{
+	return NULL;
+}
+
+
--- utility_toolkit/utility_toolkit.h
+++ utility_toolkit/utility_toolkit.h
@@ -4,3 +4,5 @@ char * base64_decoder(const char *);
 char * base64_encoder(const char *);
 char * url_decoder(const char *);
 char * url_encoder(const char *);
+char *ascii2hex(const char  *,size_t);
+char *hex2ascii(const char  *,size_t);
