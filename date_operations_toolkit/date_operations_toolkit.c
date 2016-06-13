00efde642e14bd1eb343badb1dfe6c06244128a5
0x80 <netsecdeveloper@gmail.com>
2016-06-12 20:26:33 -0700

    get system date function implemented
    Like this 06/12/16 - 08:25PM format

--- date_operations_toolkit/date_operations_toolkit.c
+++ date_operations_toolkit/date_operations_toolkit.c
@@ -7,3 +7,23 @@ int is_leap_year(unsigned int year)
 	else 				return 0;
 
 }
+
+/*current_date length min 20 elements*/
+char * get_system_current_date(char current_date[],size_t size)
+{
+	time_t t = time(NULL);
+	struct tm *lc_time = localtime(&t);	
+
+	/*	Second way without strftime
+		lc_time->tm_year + 1900, 
+		lc_time->tm_mon + 1, 
+		lc_time->tm_mday, 
+		lc_time->tm_hour, 
+		lc_time->tm_min, 
+		lc_time->tm_sec
+	*/
+	
+	strftime(current_date, size, "%x - %I:%M%p", lc_time);
+
+	return current_date;
+}
--- date_operations_toolkit/date_operations_toolkit.h
+++ date_operations_toolkit/date_operations_toolkit.h
@@ -1,1 +1,2 @@
 int is_leap_year(unsigned int);
+char * get_system_current_date(char [],size_t );
