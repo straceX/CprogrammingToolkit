563b412a0e49f1428ce764263a818994472c5d99
straceX <netsecdeveloper@gmail.com>
2016-06-14 20:03:03 +0300

    swap function like macro added

--- utility_toolkit/utility_toolkit.h
+++ utility_toolkit/utility_toolkit.h
@@ -1,3 +1,23 @@
+#ifdef OS_WINDOWS
+  
+   
+
+#else
+
+ #define SWAP(valX,valY)                              \
+        {                                             \
+          typeof (valX) valZ;                         \
+          valZ = valX;                                \
+          valX = valY;                                \
+          valY = valZ;                                \
+        }
+
+
+
+
+#endif
+
+
 void swap2int(int *,int *);
 int inverse_number(int);
 char * base64_decoder(const char *);
