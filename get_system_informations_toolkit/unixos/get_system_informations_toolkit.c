17784ac7d7464ee5e08fce9c51b4b9f141e1af44
straceX <netsecdeveloper@gmail.com>
2016-06-15 02:23:45 +0300

    get system netmask address [Linux]

--- get_system_informations_toolkit/unixos/get_system_informations_toolkit.c
+++ get_system_informations_toolkit/unixos/get_system_informations_toolkit.c
@@ -54,6 +54,29 @@ char *get_system_mac(char *ptrMac,const char *interface)
     return ptrMac;
 }
 
+char *get_system_netmask(const char *interface)
+{
+	 int ptrSfd;
+	 struct ifreq ifr;
+
+	 ptrSfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	 if(ptrSfd == -1)
+	 	return NULL;
+	 else
+	 {	 
+	 	ifr.ifr_addr.sa_family = AF_INET;
+	 
+	 	strncpy(ifr.ifr_name, interface, IFNAMSIZ-1);
+
+	 	ioctl(ptrSfd, SIOCGIFNETMASK, &ifr);
+
+	 	close(ptrSfd);
+
+	 	return inet_ntoa( ( (struct sockaddr_in *)(&ifr.ifr_addr) )->sin_addr );
+	 }
+}
+
 /*free(return memory address) after use*/
 char *getpwd(void)
 {
--- get_system_informations_toolkit/unixos/get_system_informations_toolkit.h
+++ get_system_informations_toolkit/unixos/get_system_informations_toolkit.h
@@ -15,5 +15,6 @@
 int get_system_cpu_arc();
 char *get_system_ip(char *);
 char *get_system_mac(char *,const char*);
+char *get_system_netmask(const char *interface);
 char *getpwd(void);
 char *getcwd(void);
