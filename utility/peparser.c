63fb3c84209c7b7536b278d6279337155939b513
int 0x80 <netsecdeveloper@gmail.com>
2017-02-18 14:10:36 +0300

    pe parser function template added
    pe parser function template added

---
+++ utility/peparser.c
@@ -0,0 +1,41 @@
+#include <peparser.h>
+
+void print_image_file_header(void)
+{
+
+}
+
+void print_image_optional_file_header(void)
+{
+
+}
+
+void print_image_section_headers(void)
+{
+
+}
+
+void print_export_directory(void)
+{
+
+}
+
+void print_import_directory(void);
+{
+
+}
+
+DWORD RVA_to_file_offset(DWORD rva)
+{
+
+}
+void exit_sys(LPCSTR lpszMsg, int status)
+{
+
+}
+
+void exit_usr(LPCSTR lpszMsg, int status)
+{
+
+}
+
