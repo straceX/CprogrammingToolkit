ec6600e61b3fd80b607572dd8f8ba91e6c2b0ad3
int 0x80 <netsecdeveloper@gmail.com>
2017-04-01 19:07:45 +0300

    HTTP Client Function added Header file
    HTTP Client Function added Header file

--- network_operations/unixos/network_operations_toolkit.h
+++ network_operations/unixos/network_operations_toolkit.h
@@ -1,2 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+	
 int open_tcpip_server(int);
 int open_tcpip_client(char *,int);
+
+
+
+void err_exit(const char *msg);
+int open_connetion(const char *hostname, const int portno,const char **msgs);
+int read_line_unbuffered(int sock, char *buf, size_t len);
+int read_socket(int sock, void *buf, size_t len);
+int get_file_via_http(const char *hostname, const char *portno, const char *filename);
+
+
+
+
+
