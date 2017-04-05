e37067656b663849cfafe6aa10e3a39b8090424b
0x80 <netsecdeveloper@gmail.com>
2017-04-05 22:36:37 +0300

    windows http request function added header file
    windows http request function added header file

--- network_operations/windowsos/network_operations_toolkit.h
+++ network_operations/windowsos/network_operations_toolkit.h
@@ -1,2 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <WinSock2.h>
+#include <Windows.h>
+
+/*------------------------------------------------------------------------------------*/
+void err_exit(LPCSTR lpszMsg, int status, DWORD dwLastError);
+int read_line_unbuffered(SOCKET sock, char *buf, size_t len);
+int read_socket(SOCKET sock, void *buf, size_t len);
+int open_connetion(const char *hostname, const int portno, const char **msgs);
+int get_file_via_http(const char *hostname, const char *portno, const char *filename);
+/*------------------------------------------------------------------------------------*/
+
+
+/*------------------------------------------------------------------------------------*/
 int open_tcpip_server(int);
 int open_tcpip_client(char *,int);
+/*------------------------------------------------------------------------------------*/
